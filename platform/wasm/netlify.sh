65db61a6d0ba3f92cc65ff1331db259d2563f83b
iphydf <iphydf@users.noreply.github.com>
2025-02-06 22:17:40 +0000

    chore: Add ccache to netlify build.
    Speeding up the build and saving build minutes.

--- platform/wasm/netlify.sh
+++ platform/wasm/netlify.sh
@@ -4,10 +4,28 @@ set -eux -o pipefail
 
 PROJECT=debugtox
 
-tar -C /opt -zxf <(curl -L https://github.com/TokTok/dockerfiles/releases/download/nightly/qtox-wasm-buildhome.tar.gz)
+if [ ! -f /opt/buildhome/bin/ccache ]; then
+  mkdir -p /opt/buildhome/bin
+  tar -C /opt/buildhome/bin --strip-components=1 -Jxf \
+    <(curl -L https://github.com/ccache/ccache/releases/download/v4.10.2/ccache-4.10.2-linux-x86_64.tar.xz) \
+    ccache-4.10.2-linux-x86_64/ccache
+fi
+if [ ! -d /opt/buildhome/emsdk ]; then
+  tar -C /opt -zxf \
+    <(curl -L https://github.com/TokTok/dockerfiles/releases/download/nightly/qtox-wasm-buildhome.tar.gz)
+fi
 
 . '/opt/buildhome/emsdk/emsdk_env.sh'
+export PATH="/opt/buildhome/bin:$PATH"
 export PKG_CONFIG_PATH='/opt/buildhome/lib/pkgconfig'
+
+# Limit ccache to 100MB storage.
+ccache -M 100M
+ccache --show-stats
+ccache --zero-stats
+
+export CC='ccache emcc'
+export CXX='ccache em++'
 
 emcmake cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -16,6 +34,8 @@ emcmake cmake \
   -H.
 
 cmake --build _build-wasm --parallel "$(nproc)"
+
+ccache --show-stats
 
 mkdir _site
 
